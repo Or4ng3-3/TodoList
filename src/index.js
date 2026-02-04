3b0a8de8b231db066d8cc9b97ba2827b43d453c8
Or4ng3 <imor4ng3@gmail.com>
2026-02-04 20:10:53 +0800

    added routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+app.get('/reminders', (req, res) => {
+    res.send('Get all reminders')
+}) 
+
+app.get('/reminders/:id', (req, res) => {
+    res.send('Get single reminder by id')
+}) 
+
+app.post('/reminders', (req, res) => {
+    res.send('create a new reminder')
+}) 
+
+app.patch('/reminders/:id', (req, res) => {
+    res.send('update a reminder')
+}) 
+
+app.delete('/reminders', (req, res) => {
+    res.send('delete a reminder')
 }) 
 
 app.listen(port, () => {
